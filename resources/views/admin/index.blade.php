69c1c781d84e853420abee4b4f43c1cbf12232f4
xxkinaxx <ekopandu088@gmail.com>
2024-02-16 14:52:03 +0700

    changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,4 +6,24 @@
     <a href="{{ route('admin.create') }}" class="btn btn-primary">Create Admin</a>
 </div>
 
+<div class="container">
+    <h5 class="fw-bold">Users</h5>
+    <table class="table">
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Action</th>
+        </th>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</div>
+
 @endsection
